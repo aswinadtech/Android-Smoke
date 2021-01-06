4a640399e8814fe56d3954a1d747381aef92de08
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2021-01-06 10:30:37 +0530

    Update smokeTestCases.java

--- src/test/java/twc/Automation/SmokeTestCases/smokeTestCases.java
+++ src/test/java/twc/Automation/SmokeTestCases/smokeTestCases.java
@@ -120,7 +120,7 @@ public class smokeTestCases extends  TwcAndroidBaseTest {
 	@Title("Verifying feed_2 ad call on FTL")
 	public void Smoke_Test_verifying_feed_2adcall_FTL() throws Exception {
 		System.out.println("================= Verifying feed_2 ad call tescase Started =========================");
-		//Functions.verifying_feedcalls(2);
+		Functions.verifying_feedcalls(2);
 		System.out.println("================= Verifying feed_2 ad call tescase End =========================");
 	}
 
@@ -129,7 +129,7 @@ public class smokeTestCases extends  TwcAndroidBaseTest {
 	@Title("Verifying feed_3 ad cal on FTL")
 	public void Smoke_Test_verifying_feed_3adCall_FTL() throws Exception {
 		System.out.println("================= Verifying feed_3 ad call tescase Started =========================");
-		//Functions.verifying_feedcalls(3);
+		Functions.verifying_feedcalls(3);
 		System.out.println("================= Verifying feed_3 ad call tescase End =========================");
 	}
 
@@ -137,7 +137,7 @@ public class smokeTestCases extends  TwcAndroidBaseTest {
 	@Title("Verifying feed_4 ad call on FTL")
 	public void Smoke_Test_verifying_feed_4adcall_FTL() throws Exception {
 		System.out.println("================= Verifying feed_4 ad call tescase Started =========================");
-		//Functions.verifying_feedcalls(4);
+		Functions.verifying_feedcalls(4);
 		System.out.println("================= Verifying feed_4 ad call tescase End =========================");
 	}
 
@@ -146,7 +146,7 @@ public class smokeTestCases extends  TwcAndroidBaseTest {
 	@Title("Verifying feed_5 ad call on FTL")
 	public void Smoke_Test_verifying_feed_5adCall_FTL() throws Exception {
 		System.out.println("================= Verifying feed_5 ad call tescase Started =========================");
-		//Functions.verifying_feedcalls(5);
+		Functions.verifying_feedcalls(5);
 		System.out.println("================= Verifying feed_5 ad call tescase End =========================");
 	}
 
