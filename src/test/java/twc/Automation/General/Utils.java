439dd1d908fa8804b26916ac7138157c312e3d93
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2021-09-03 15:16:21 +0530

    Update Utils.java

--- src/test/java/twc/Automation/General/Utils.java
+++ src/test/java/twc/Automation/General/Utils.java
@@ -84,8 +84,8 @@ public class Utils extends Functions {
 	public static boolean unlimitedInterstitial = false;
 	public static String Exception = null;
 	public static int feedAdCount = 0;
-	//public static String videoIUValue = "iu=%2F7646%2Fapp_android_us%2Fvideo";
-public static String videoIUValue = "iu=%2F7646%2Fapp_android_us%2Fweather%2Fsevere%2Ftropical";
+	public static String videoIUValue = "iu=%2F7646%2Fapp_android_us%2Fvideo";
+//public static String videoIUValue = "iu=%2F7646%2Fapp_android_us%2Fweather%2Fsevere%2Ftropical";
 	//public static String videoIUValue =null;
 	public static String iuId = null;
 	public enum CardNames {
